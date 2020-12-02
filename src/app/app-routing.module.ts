69fd623a9238e8b8b673cf537daac949ec894ec0
Oscar <ogomezsts51l@gmail.com>
2020-12-02 12:43:27 +0100

    angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {HomeComponent} from './home/home.component';
+import {PerfilComponent} from './perfil/perfil.component';
+import {LoginComponent} from './login/login.component';
+import {RegistroComponent} from './registro/registro.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'profile', component: PerfilComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegistroComponent },
+  { path: '**', redirectTo:'', pathMatch:'full'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
